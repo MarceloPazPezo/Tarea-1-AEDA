346f83dd657802f290f1a9d7346af746dcae72c9
VictorLopezG <victor.lopez2101@alumnos.ubiobio.cl>
2025-04-30 18:23:52 -0400

    Busqueda Galopante probada

--- B_Galopante.cpp
+++ B_Galopante.cpp
@@ -1,37 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+using namespace std;
 
-int bBinaria(int A[],int linf,int size,int x){
-    
-    if(A[linf+(size/2)==x]){
 
-        return linf+(size/2);
-
-    }else if(A[linf+(size/2)<x]){
-
-        linf=linf+(size/2);
-        size=size/2;
-
-        return bBinaria(A,linf,size,x);
-
-    }else if(A[size+(linf/2)>x]){
-        size=size/2;
-        return bBinaria(A,linf,size,x);
-    }
-}
-
-int bGalopante(int A[],int size,int x){
-
+int bBinaria(short A[], int linf, int size, int x)
+{
     if(size==0){
         return -1;
+    }
+    if (A[linf + (size / 2)]==x)
+    {
+        return linf + (size / 2);
+    }
+    if (A[linf + (size / 2)]<x)
+    {
+        linf = linf + (size / 2);
+        size = size / 2;
+        return bBinaria(A, linf+1, size, x);
     }else{
-        int linf=1;
-        while(linf*2<size && A[linf*2]<x){
-            linf=linf*2;
-        }
-        return bBinaria(A,linf,size,x);
+        size = size / 2;
+        return bBinaria(A, linf, size, x);
     }
 }
 
-int main(){
+int bGalopante(short A[], int size, int x)
+{
+    if (size == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        int linf = 1;
+        while (linf * 2 < size && A[linf * 2] < x)
+        {
+            linf = linf * 2;
+        }
+        return bBinaria(A, linf-1, size, x);
+    }
+}
 
+int main()
+{
+    int tam = 10;
+    short Arr[tam] = {1, 2, 3, 5, 7, 11, 13, 17, 19, 23};
+    int valR =5;
+    int res = bGalopante(Arr, tam, valR);
+    cout << "Valor encontrado en la posicion:" <<res;
+    return 0;
 }
