864427b17ce26f9bd4fb57c7ceaf3823c1472b4f
VictorLopezG <victor.lopez2101@alumnos.ubiobio.cl>
2025-05-01 15:09:21 -0400

    [FIX] se arreglo error de busqueda en los extremos, se implemento metodo de pruebas

--- B_Galopante.cpp
+++ B_Galopante.cpp
@@ -2,26 +2,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <iostream>
+#include <chrono>
 using namespace std;
-
 
 int bBinaria(short A[], int linf, int size, int x)
 {
-    if(size==0){
+    // cout<<linf + (size / 2)<<endl;
+    if (size == 0)
+    {
         return -1;
     }
-    if (A[linf + (size / 2)]==x)
+    if (A[linf + (size / 2)] == x)
     {
         return linf + (size / 2);
     }
-    if (A[linf + (size / 2)]<x)
+    if (A[linf + (size / 2)] < x)
     {
-        linf = linf + (size / 2);
-        size = size / 2;
-        return bBinaria(A, linf+1, size, x);
-    }else{
-        size = size / 2;
-        return bBinaria(A, linf, size, x);
+        return bBinaria(A, linf + (size / 2) + 1, size / 2, x);
+    }
+    else
+    {
+        return bBinaria(A, linf, size / 2, x);
     }
 }
 
@@ -38,16 +39,45 @@ int bGalopante(short A[], int size, int x)
         {
             linf = linf * 2;
         }
-        return bBinaria(A, linf-1, size, x);
+        return bBinaria(A, linf - 1, size - linf, x);
     }
 }
 
 int main()
 {
-    int tam = 10;
-    short Arr[tam] = {1, 2, 3, 5, 7, 11, 13, 17, 19, 23};
-    int valR =5;
+    int tam = 10000;//tamaño del arreglo
+    short Arr[tam]; //= {1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59};
+    unsigned t0, t1;
+    short val;
+    for (int i = 0; i < tam; i++)
+    {
+        val = short(rand() % ((i + 1) * 3));//llenado de randoms en orden
+        if (val > Arr[i - 1] || i == 0)
+        {
+            Arr[i] = val;
+            // cout<<val<<" ";
+        }
+        else
+        {
+            i--;
+        }
+    }
+    cout << endl;
+    int valR = int(rand() % tam);
+    cout << "posicion: " << valR << endl;
+    valR = Arr[valR];
+    cout << "valor: " << valR << endl;
+    t0 = clock();
     int res = bGalopante(Arr, tam, valR);
-    cout << "Valor encontrado en la posicion:" <<res;
+    t1 = clock();
+    double tiempo = double((t1 - t0)  / CLOCKS_PER_SEC);
+    if (res == -1)
+    {
+        cout << "Valor no encontrado, tiempo: " << tiempo << endl;
+    }
+    else
+    {
+        cout << "El valor " << valR << " fue encontrado en la posicion:" << res << " tiempo: " << tiempo << " ms" << endl;
+    }
     return 0;
 }
